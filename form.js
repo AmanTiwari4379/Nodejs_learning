5b5f75d5487b4c297072d9aa9e513c734dcfdd37
Aman Tiwari <taman4379@gmail.com>
2026-04-01 12:04:23 +0530

    form.js is main file to create user data file from browser input

--- form.js
+++ form.js
@@ -1,13 +1,33 @@
 const http = require('http');
+const fs = require('fs');
+const querystring = require('querystring');
 
-http.createServer((req, res)=> {
-    res.writeHead(200, {'content-type': 'text/html'});
-    if(req.url == '/'){
-        res.write(`
-        `);
-    }else if(req.url == '/submit'){
-        res.write('<h2>Data Submitted</h2>');
-    }
-    
-    res.end();
+http.createServer((req, res) => {
+    fs.readFile('html/form.html', 'utf-8', (error, data) => {
+        if (error) {
+            res.writeHead(500, { 'content-type': 'text/plain' })
+            res.end('internal server error')
+            return;
+        }
+        res.writeHead(200, { 'content-type': 'text/html' })
+        if (req.url == '/') {
+            res.write(data);
+        } else if (req.url == '/submit') {
+            let dataBody = [];
+            req.on('data', (ch) => {
+                dataBody.push(ch);
+            });
+            req.on('end', ()=> {
+                let rawData = Buffer.concat(dataBody).toString();
+                let readableData = querystring.parse(rawData);
+                let dataString = "My name is "+ readableData.name+ " and my email id is "+ readableData.email; 
+                console.log(dataString);
+                fs.writeFileSync("text/"+readableData.name+".txt", dataString);
+                console.log('file created');
+            });
+            res.write('<h2> Data Submitted</h2>')
+        }
+        res.end()
+    })
+
 }).listen(3000);
