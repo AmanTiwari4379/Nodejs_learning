50201cd45d17f7d0202e05874df9dc1559d602a2
Aman Tiwari <taman4379@gmail.com>
2026-05-17 11:34:47 +0530

    Using html and css files to display on url request

--- website.js
+++ website.js
@@ -1,14 +1,28 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res)=>{
-    fs.readFile("html/bookListApp.html", "utf-8", (error, data)=>{
-        if(error){
-            res.writeHead(500, {"content-type": "text/plain"})
-            res.end("internal server error")
-            return false
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      fs.readFile("html/index.html", "utf-8", (error, data) => {
+        if (error) {
+          res.writeHead(500, { "content-type": "text/plain" });
+          res.end("internal server error");
+          return false;
         }
         res.write(data);
         res.end();
-    })
-}).listen(3200);
+      });
+    } else if (req.url == "/style.css") {
+      fs.readFile("html/style.css", "utf-8", (error, data) => {
+        if (error) {
+          res.writeHead(500, { "content-type": "text/plain" });
+          res.end("css not found");
+          return false;
+        }
+        res.writeHead(200, { "content-type": "text/css" });
+        res.end(data);
+      });
+    }
+  })
+  .listen(3200);
